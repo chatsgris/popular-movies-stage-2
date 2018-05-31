47c916f4f3bb6d565618e753db7934d8627f9d42
chatsgris <liumiru94@gmail.com>
2018-05-30 22:07:18 -0700

    set up data folder

--- app/src/main/java/com/android/popularmoviesstage1/data/MoviesContract.java
+++ app/src/main/java/com/android/popularmoviesstage1/data/MoviesContract.java
@@ -2,9 +2,9 @@ package com.android.popularmoviesstage1.data;
 
 import android.provider.BaseColumns;
 
-public class FavoriteContract {
-    public static final class FavoriteEntry implements BaseColumns {
-        public static final String TABLE_NAME = "favorites";
+public class MoviesContract {
+    public static final class MovieEntry implements BaseColumns {
+        public static final String TABLE_NAME = "tableFavorites";
         public static final String COLUMN_MOVIE_DATA = "movieData";
         public static final String COLUMN_TIMESTAMP = "timeStamp";
     }
---
+++ app/src/main/java/com/android/popularmoviesstage1/data/MoviesDbHelper.java
@@ -0,0 +1,30 @@
+package com.android.popularmoviesstage1.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MoviesDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "movies.db";
+    private static final int DATABASE_VERSION = 3;
+
+    public MoviesDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_CREATE_MOVIES_TABLE = "CREATE TABLE " + MoviesContract.MovieEntry.TABLE_NAME + " (" +
+                MoviesContract.MovieEntry._ID + " INT NOT NULL, " +
+                MoviesContract.MovieEntry.COLUMN_MOVIE_DATA + " TEXT NOT NULL, " +
+                MoviesContract.MovieEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
+                "); ";
+        sqLiteDatabase.execSQL(SQL_CREATE_MOVIES_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + MoviesContract.MovieEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
